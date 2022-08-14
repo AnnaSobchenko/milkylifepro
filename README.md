4574fb5d5ed5a46d9d6dc652e67c73d4c8888f7e
AnnaSobchenko <annsbchnk@gmail.com>
2022-08-14 21:56:19 +0300

    mamamarafon update

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # milkylifepro
+
 The site for mums who need information and help in feeding and raising
 a child in the first years of life;
 
-<h3>MILKYLIFEPRO ( Front-End repository )</h3>
- Front-End (HTML, SASS, JS, React, Redux, Router-Dom, Axios)
-<h3>MILKYLIFEPRO-REST-API (https://github.com/AnnaSobchenko/milkylifepro-rest-api)</h3>
+<h3>MilkyLifePro ( Front-End repository )</h3>
 
- Back-End (NodeJS, Mongoose, Joi, Sendgrid, Gravatal, Swagger, MongoDB) 
- 
-- tree role on site (guest, verify user, admin); 
-- adaptive design (mobile first); 
-- dark/light theme; 
+ Front-End (HTML, SASS, JS, React, Redux, Router-Dom, Axios, Formik, Dotenv, Uuid)
+
+
+<h3>MilkyLifePro-REST-API (https://github.com/AnnaSobchenko/milkylifepro-rest-api)</h3>
+
+Back-End (NodeJS, Mongoose, Dotenv, Joi, Bcryptjs, Sendgrid, Jimp, Multer, Gravatar, Swagger, MongoDB)
+
+
+- tree role on site (guest, verify user, admin);
+- adaptive design (mobile first);
+- dark/light theme;
 - backend documentation - swagger;
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,33 +1,43 @@
 import s from "./About.module.scss";
+import { NavLink } from "react-router-dom";
 
 const Photo = require("../../images/foto1.jpg");
 
 const About = () => {
-    return ( 
-        <section className={`container ${s.about }`} id="about">
-          <div className={s.img}>
-            <img src={Photo} alt="replacePhoto" width={450} />            
-            </div>
-          <div className={s.text__welcome}>
-            <p className={s.text}>
-              Привіт, <br /> я сертифікована консультантка із грудного
-              вигодовування та експертка із дитячої психології по теорії Гордона
-              Н'юфелда (Канада). Крім цього, я розробила унікальну подушку для
-              відновлення після пологів і для годуючих мам.
-            </p>
-            <p className={s.text}>
-              До кар'єри консультантки я працювала керівником лабораторії в
-              Києво-Могилянській академії.
-            </p>
-            <p className={s.text}>
-              Зі мною ви можете бути впевнені, що грудне вигодовування принесе
-              тільки задоволення. З радістю допоможу вам знайти відповіді на
-              будь-які питання і розв'язати проблеми будь-якої складності.
-            </p>
-            <p className={s.text}> Ваша, Олеся Форемна</p>
-          </div>
-        </section>
-     );
-}
- 
+  return (
+    <section className={`container ${s.about}`} id="about">
+      <div className={s.img}>
+        <img src={Photo} alt="replacePhoto" width={450} />
+      </div>
+      <div className={s.text__welcome}>
+        <p className={s.text}>
+          Привіт, <br /> я сертифікована консультантка з грудного вигодовування.
+          Навчалася в Інституті Гордона Ньюфелда (Канада) де вивчала дитячу
+          психологію.
+        </p>
+        <p className={s.text}>
+          Мама двох донечок, яких годувала і годую грудьми тому розумію кожне
+          ваше хвилювання і ваші потреби. Зі мною ви можете бути впевнені, що
+          грудне вигодовування принесе тільки задоволення. З радістю допоможу
+          вам знайти відповіді на будь-які питання і розв'язати проблеми
+          будь-якої складності.
+        </p>
+        <p className={s.text}>
+          Авторка та кураторка &nbsp;
+          <NavLink to="/mama-marafon">"Мамо, марафон!"</NavLink>
+        </p>
+        <p className={s.text}>
+          Крім цього, я розробила унікальну подушку для відновлення після
+          пологів і для годуючих мам.
+        </p>
+        <p className={s.text}>
+          Буду рада із вами поспілкуватися. Якщо виникли питання пишіть мені в  &nbsp;
+          <a rel="noopener noreferrer" target="_blank" href="https://t.me/Olesia_Foremna">Телеграм</a>
+        </p>
+        <p className={s.text}> Ваша, Олеся Форемна</p>
+      </div>
+    </section>
+  );
+};
+
 export default About;
