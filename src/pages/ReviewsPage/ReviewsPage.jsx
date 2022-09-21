c5997218cb5c1ffe0b54fd0635037744fb5fb5c8
AnnaSobchenko <annsbchnk@gmail.com>
2022-09-21 23:43:41 +0300

    add reviews page

--- src/pages/ReviewsPage/ReviewsPage.jsx
+++ src/pages/ReviewsPage/ReviewsPage.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getReviews } from "../../redux/reviews/reviewsOperations";
+import { getAllReviews } from "../../redux/reviews/reviewsSelector";
 import s from "./ReviewsPage.module.scss";
 
 const ReviewsPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getReviews());
+  }, []);
+  const Reviews = useSelector(getAllReviews);
   return (
     <div className={`container ${s.reviews}`}>
-      <h1>ReviewsPage</h1>
+      <ul>
+        {Reviews.map((el) => (
+          <li key={el._id}>
+            <p>"{el.review}"</p>
+            <p className={s.item__user}>{el.user}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
